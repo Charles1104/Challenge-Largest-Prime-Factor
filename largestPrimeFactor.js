8a251526ff64b16c2ef9f6504d42f54ecb0fb6fc
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-16 09:50:54 -1000

    Prime array

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,18 @@
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+
+  var primeNumber = [];
+
+    for(var i = 2; i < n; i++) {
+      for (var j = 2 ; j < i; j++){
+        if(i % j === 0) {
+          break;
+        }
+        primeNumber.push(i);
+      }
+    }
 
   return primeNumber;
+
 };
+
+console.log(largestPrimeFactor(20));
